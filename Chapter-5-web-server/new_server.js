732aafece8576881b20a729bc46364a648316e43
thefireatom <ivaner1234@yandex.ru>
2024-01-06 12:40:01 +0300

    Guide files

---
+++ Chapter-5-web-server/new_server.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+
+const logEvents = require("./logEvents")
+const EventEmitter = require("events");
+class Emitter extends EventEmitter { };
+// initialize object 
+const myEmitter = new Emitter();
+
+
+
+
+
+
+// myEventEmitter.on("log", (msg) => logEvents(msg));
+//     myEmitter.emit("log", "Log event emitted!");
