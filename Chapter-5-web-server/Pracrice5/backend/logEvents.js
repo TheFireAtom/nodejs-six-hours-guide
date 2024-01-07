3b8a011f10f985df1745fd4b2c070b716055c43c
thefireatom <ivaner1234@yandex.ru>
2024-01-07 13:14:37 +0300

    Done with chapter five theory, now gonna practise

---
+++ Chapter-5-web-server/Pracrice5/backend/logEvents.js
@@ -0,0 +1,13 @@
+// logEvents module
+
+// Requirements
+//(npm libraries)
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+// (nodejs libraries)
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs").promises;
+
+
+const logEvents = async(filePath, )
