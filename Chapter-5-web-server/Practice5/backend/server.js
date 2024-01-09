ac502ce233c5326ac520c452941b4b374b5e2bf2
thefireatom <ivaner1234@yandex.ru>
2024-01-09 23:15:13 +0300

    some server code that doesn't how i want (im really tired with this directory thing going on right now)

--- Chapter-5-web-server/Practice5/backend/server.js
+++ Chapter-5-web-server/Practice5/backend/server.js
@@ -9,16 +9,57 @@ const fsPromises = require('fs').promises;
 // other requirements 
 const { EventEmitter } = require("events");
 
-// main class creation
+// server, server info and server listener
 const eventEmitter = new EventEmitter();
 const PORT = 3000;
+const frontDir = "/home/thefireatom/Documents/Coding_Projects/nodejs-six-hours-guide/Chapter-5-web-server/Practice5/frontend";
+// const frontDirHTML = "/home/thefireatom/Documents/Coding_Projects/nodejs-six-hours-guide/Chapter-5-web-server/Practice5/frontend/html";
+// const frontDirCSS = "/home/thefireatom/Documents/Coding_Projects/nodejs-six-hours-guide/Chapter-5-web-server/Practice5/frontend/css";
+// const filePath = path.join(__dirname, fileName());
 const server = http.createServer((req, res) => {
-    req.statusCode = 200;
-    req.writeHead({"Content-Type": "text/plain"});
-    res.end()
+
+    const filePath = path.join(frontDir, req.url === "/" ? `/html/main.html` : req.url);
+    const extName = path.extname(filePath).toLowerCase();
+    let contentType;
+    switch (extName) {
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".jpeg":
+            contentType = "image/jpeg";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        case ".gif":
+            contentType = "image/gif";
+            break;
+        default:
+            contentType = "text/html";
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.statusCode = 404;
+            console.error("Error has occured: ", err);
+            res.end();
+        } else {
+            res.setHeader(200, { "Content-Type": contentType});
+            res.end(content, "utf8");
+        }
+    });
+
 });
 
 server.listen(PORT, () => {
-    console.log("Server is currently running at port ", PORT);
+    console.log(`Server is currently running at: http://localhost${PORT}/`);
 });
 
+console.log(__dirname);
