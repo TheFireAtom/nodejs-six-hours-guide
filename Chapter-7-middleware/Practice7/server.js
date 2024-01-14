3768a212a35af3bb3dd60e01d93f6c67ac98ca02
thefireatom <ivaner1234@yandex.ru>
2024-01-14 18:00:24 +0300

    logEvents doesnt work (need to fix it somehow)

--- Chapter-7-middleware/Practice7/server.js
+++ Chapter-7-middleware/Practice7/server.js
@@ -5,7 +5,26 @@ const path = require("path");
 const PORT = process.env.PORT || 3500;
 const htmlPath = path.join(__dirname, "public", "html");
 const cssPath = path.join(__dirname, "public", "css");
+const { logger } = require("./middleware/logEvents");
+const cors = require("cors");
 
+// Cross Origin Resource Sharing
+
+app.use(logger);
+
+const whiteList = ["http://127.0.0.1:5500", "https://www.google.com", "http://localhost:3500/"];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+ 
 // get methods
 app.get("^/$|/main(.html)?", (req, res) => {
     res.sendFile(path.join(htmlPath, "main.html"));
@@ -42,6 +61,19 @@ app.use(express.json());
 // for handling urlencoded data
 
 app.use(express.urlencoded({ extended: false }))
+
+// app.all
+
+app.all("*", (req, res) => {
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(htmlPath, "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({ "error": "404 Not Found"});
+    } else if (req.accepts("txt")) {
+        res.type("txt").send("404 Not Found");
+    }
+});
 
 // app listener
 
