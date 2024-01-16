112baf5a0e6e20c92d4ace9bd9b2bb4339338653
thefireatom <ivaner1234@yandex.ru>
2024-01-16 19:32:02 +0300

    almost done with practice 9 (onle need to understand how to work with updating json in js)

---
+++ Chapter-9-MVC-REST-API/Practice9/controllers/employeesController.js
@@ -0,0 +1,46 @@
+const data = {
+    employees: require("../data/employees.json"),
+    setEmployees: function (data) { this.employees = data }
+}
+
+const getAllEmployees = (req, res) => {
+    res.json(data.employees);
+}
+
+const createNewEmployee = (req, res) => {
+    const newEmployee = {
+        id: data.employees[data.employees.id - 1].id + 1 || 1,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname
+    }
+
+    if (!req.body.firstname || !req.body.lastname) {
+        return res.status(400).json({"message": "Provide firstname and lastname of the employee"})
+    }
+
+    data.setEmployees([...data.employees, newEmployee]);
+    res.status(201).json(data.employees);
+}
+
+const updateEmployee = (req, res) => {
+    res.json({
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname
+    });
+}
+
+const deleteEmployee = (req, res) => {
+    res.json({ "id": req.body.id });
+}
+
+const getEmployee = (req, res) => {
+    res.json({ "id": req.params.id });
+}
+
+module.exports = { 
+    getAllEmployees, 
+    createNewEmployee, 
+    updateEmployee, 
+    deleteEmployee, 
+    getEmployee 
+}
