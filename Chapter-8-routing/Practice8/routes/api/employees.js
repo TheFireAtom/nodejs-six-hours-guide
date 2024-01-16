7e09b49ed9c79edbdfbdeb8d36180c8c13104e43
thefireatom <ivaner1234@yandex.ru>
2024-01-16 12:04:59 +0300

    change post method for pushing new employee

--- Chapter-8-routing/Practice8/routes/api/employees.js
+++ Chapter-8-routing/Practice8/routes/api/employees.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.resolve(__dirname, "../../data/employees.json");
 const data = {};
-data.employees = require("../../data/employees.json");
+data.employees = require(filePath);
 
 router.route("/")
     .get((req, res) => {
@@ -9,10 +13,21 @@ router.route("/")
     })
 
     .post((req, res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        });
+        const newEmployee = {
+            "id": data.employees.length + 1,
+            "firstname": "the",
+            "lastname": "fire"
+        }
+
+        // if (!data.employees || !Array.isArray(data.employees)) {
+        //     data.employees = [];
+        // }
+
+        data.employees.push(newEmployee);
+
+        fs.writeFileSync(filePath, JSON.stringify(data.employees, null, 2));
+
+        res.json(newEmployee);
     })
 
     .put((req, res) => {
@@ -29,6 +44,11 @@ router.route("/")
 router.route("/:id")
     .get((req, res) => {
         res.json({ "id": req.params.id });
+    })
+
+    .delete((req, res) => {
+        const nowId = req.params.id;
+        data.employees.splice(nowId, 1);
     });
 
 module.exports = router;
